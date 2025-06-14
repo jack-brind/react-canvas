76d531fc27e72a7517fcdc07111fde4ca5ab62ee
Jack Brind <63796862+jack-brind@users.noreply.github.com>
2025-06-14 07:22:52 +0100

    Added event countdown component

--- src/components/TestComponents/EventCountdown/EventCountdown.jsx
+++ src/components/TestComponents/EventCountdown/EventCountdown.jsx
@@ -1,13 +1,91 @@
 import "./EventCountdown.css";
 import PageContent from "../../PageContent/PageContent.jsx";
 import pages from "../../../pages.js";
-// import { events } from "./Events.js";
+import { eventsAscending } from "./Events.js";
+import { differenceInCalendarDays, format } from "date-fns";
+import { Star, RefreshCcw } from "lucide-react";
+import { IconMobiledata, IconRotateClockwise2 } from "@tabler/icons-react";
 
-// ========
-// Create components here.active
-// ========
+import { useState } from "react";
 
-function Buttons() {
+// Day calculation helper
+function getDays(date) {
+  const daysUntil = differenceInCalendarDays(date, new Date());
+  return daysUntil;
+}
+
+function Events() {
+  return (
+    <div className="event__rail">
+      {eventsAscending.map((event) => (
+        <Event
+          key={event.eventName}
+          name={event.eventName}
+          date={event.date}
+          days={getDays(event.date)}
+          categories={event.categories}
+          photo={event.photo}
+          repeats={event.repeats}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Event({ name, date, days, categories, photo, repeats }) {
+  const [showDate, setShowDate] = useState(false);
+
+  return (
+    <div className={`event__container ${days < 0 ? "disabled" : ""}`}>
+      <div>
+        <img src={photo} />
+        <button className="toggle">
+          <Star />
+        </button>
+      </div>
+      <div className="content">
+        <div className="event__details">
+          <div className="event__name">
+            <p className="name">{name}</p>
+            {repeats && (
+              <div className="pill">
+                <IconRotateClockwise2 />
+                Repeats
+              </div>
+            )}
+          </div>
+          <div className="event__date">
+            <div className="days-date">
+              {showDate ? (
+                <p className="date">{format(date, "dd MMMM yyyy")}</p>
+              ) : (
+                <p className="days">
+                  {days === 0
+                    ? "Today"
+                    : days > 0
+                      ? `In ${days} day${days > 1 ? "s" : ""}`
+                      : `${Math.abs(days)} day${Math.abs(days) > 1 ? "s" : ""} ago`}
+                </p>
+              )}
+              <button onClick={() => setShowDate(!showDate)}>
+                <IconMobiledata />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="categories">
+          {categories.map((category, index) => (
+            <div className="pill" key={index}>
+              {category}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function EventCountdown() {
   const currentPageData = pages.find((page) => page.link === "eventCountdown");
 
   return (
@@ -15,10 +93,11 @@ function Buttons() {
       <PageContent
         icon={<currentPageData.icon size={32} />}
         title={currentPageData.caption}
-        subtitle="This component is a series of Countdown cards that countdown to an event. This uses date-fns for calculating dates and has some filters as well. You can also favourite events to pin to a favourites section."
+        subtitle="This component is a series of Countdown cards that countdown to an event. This uses date-fns for calculating dates and eventually, will have some filters and a way to favourite cards which will be displayed in a separate section."
       />
+      <Events />
     </>
   );
 }
 
-export default Buttons;
+export default EventCountdown;
--- src/components/TestComponents/EventCountdown/Events.js
+++ src/components/TestComponents/EventCountdown/Events.js
@@ -1,3 +1,75 @@
-export const events = [1, 2, 3];
+export const eventCategories = [
+  "Holiday",
+  "Night away",
+  "Weekend away",
+  "Golf trip",
+  "Work trip",
+  "Day trip",
+  "Celebration",
+  "Public holiday",
+  "Personal",
+];
+
+export const events = [
+  {
+    eventName: "Majorca",
+    date: new Date(2025, 6, 1),
+    categories: [eventCategories[0]],
+    photo: "/events/majorca.jpg",
+    isFavourite: true,
+    repeats: false,
+  },
+  //   {
+  //     eventName: "Glastonbury festival",
+  //     date: new Date(2025, 5, 25),
+  //     categories: [eventCategories[2], eventCategories[8]],
+  //     photo: "/events/glastonbury.png",
+  //     isFavourite: false,
+  //     repeats: false,
+  //   },
+  {
+    eventName: "Christmas",
+    date: new Date(2025, 11, 25),
+    categories: [eventCategories[7]],
+    photo: "/events/christmas.jpg",
+    isFavourite: false,
+    repeats: true,
+  },
+  {
+    eventName: "K&C wedding",
+    date: new Date(2026, 5, 20),
+    categories: [eventCategories[6]],
+    photo: "/events/wedding.jpg",
+    isFavourite: false,
+    repeats: false,
+  },
+  {
+    eventName: "Start new job",
+    date: new Date(2025, 6, 14),
+    categories: [eventCategories[6], eventCategories[8]],
+    photo: "/events/pandadoc.png",
+    isFavourite: false,
+    repeats: false,
+  },
+  {
+    eventName: "Center Parcs",
+    date: new Date(2025, 9, 10),
+    categories: [eventCategories[0]],
+    photo: "/events/centerparcs.png",
+    isFavourite: false,
+    repeats: false,
+  },
+  {
+    eventName: "Grand Theft Auto 6",
+    date: new Date(2026, 4, 26),
+    categories: [eventCategories[6], eventCategories[8]],
+    photo: "/events/gta6.png",
+    isFavourite: false,
+    repeats: false,
+  },
+];
+
+export const eventsAscending = events.toSorted((a, b) => a.date - b.date);
+export const eventsDescending = events.toSorted((a, b) => b.date - a.date);
 
 export const favouriteEvents = [];
